d448b5396a56c93f8cf47a40ecbc1524dfe5e5eb
XUE <arm@XBUNTU.(none)>
2013-11-01 22:23:53 +0800

    added sensor control ui

--- src/com/oosic/iot/controller/SensorControlActivity.java
+++ src/com/oosic/iot/controller/SensorControlActivity.java
@@ -1,8 +1,10 @@
 package com.oosic.iot.controller;
 
+import com.oosic.iot.controller.library.IotManager;
 import com.oosic.iot.controller.utils.UIUtils;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
@@ -10,14 +12,17 @@ import android.widget.TextView;
 public class SensorControlActivity extends IotBaseActivity {
 
    private static final String TAG = "IotControlActivity";
-   
-   private TextView mTempView, mHumidityView, mFanView;
+
+   private TextView mTempView, mHumidityView, mFanSwitcherView, mLightView,
+         mLightSwitcherView, mFormolView;
+   private Handler mHandler = new Handler();
+   private IotManager mIotManager;
 
    @Override
    public void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
 
-      setContentView(R.layout.activity_control);
+      setContentView(R.layout.activity_sensorcontrol);
 
       findViews();
       init();
@@ -26,17 +31,18 @@ public class SensorControlActivity extends IotBaseActivity {
    private void findViews() {
       mTempView = (TextView) findViewById(R.id.temp_sensor);
       mHumidityView = (TextView) findViewById(R.id.humidity_sensor);
-      mFanView = (TextView) findViewById(R.id.fan_switcher);
+      mFanSwitcherView = (TextView) findViewById(R.id.fan_switcher);
    }
 
    private void init() {
+      mIotManager = getIotManager();
       initViews();
    }
 
    private void initViews() {
-      mFanView.setOnClickListener(new OnClickListener() {
+      mFanSwitcherView.setOnClickListener(new OnClickListener() {
          public void onClick(View v) {
-            
+
          }
       });
    }
